2b0e8b92b243bb464edf506055c88380721c9dbb
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-10-24 15:19:58 +0900

    辞書ファイル等の日本語化

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,8 @@
 class Case < ApplicationRecord
     has_many :stations
-    accepts_nested_attributes_for :stations
+    accepts_nested_attributes_for :stations, allow_destroy: true
+    validates :title, presence: true
+    validates :charge, presence: true
+    validates :address, presence: true
+    validates :age, presence: true
 end
